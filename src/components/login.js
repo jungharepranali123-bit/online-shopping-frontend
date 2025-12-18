696f99065d9a62459446c04fc87cccda0d5b6f9c
Pranali <jungharepranali123@gmail.com>
2025-12-18 19:25:18 +0530

    Initial

---
+++ src/components/login.js
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" />
+      <br />
+      <input type="password" placeholder="Password" />
+      <br />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
